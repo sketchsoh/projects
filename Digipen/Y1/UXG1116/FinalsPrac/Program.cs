bc2aaae862c9830761a5803b74aeab54abfe0dbd
sketchsoh <sketchsoh@gmail.com>
2024-12-12 12:04:30 +0800

    Testing C++
    Testing C++ and CMake

---
+++ Digipen/Y1/UXG1116/FinalsPrac/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FinalsPrac
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> yes = new List<int>();
+
+            string s = "Hello";
+            string g = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                g += (char)((int)s[i] + 1);
+            }
+
+            int[] grades = new int[] { 10, 20, 30, 40, 50 };
+            int sum = 0;
+            for (int i = 0; i <= 5; i++)
+            {
+                sum += grades[i];
+            }
+            Console.WriteLine(sum);
+
+            Console.WriteLine(g);
+
+            int x = 10;
+            switch (x)
+            {
+                case 1:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
